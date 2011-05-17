4797b0273249a9d7b6076b68647ace781e63c8a8
David Bernal <david.bernal@gmail.com>
2011-05-17 04:30:40 -0400

    tried to store games and boards and stuff. doesn't work at all

--- local_modules/access.js
+++ local_modules/access.js
@@ -1,16 +1,40 @@
 var mongoose = require('mongoose')
 	, Schema = mongoose.Schema
 	, ObjectId = mongoose.ObjectId;
-	
+
+//	console.log(Board);
+//	console.log(Square);
+
+var Square = new Schema({
+	x: Number,
+	y: Number,
+	lit: Boolean
+});
+
+var Board = new Schema({
+	board: [[Square]]
+});
+
+var Game = new Schema({
+	title: String,
+	words: [ObjectId],
+	boards: [Board]
+});
+
+mongoose.model('Game', Game);
+
 var Word = new Schema({
-	word: String
-//	games: [Game]
+	word: String,
+	games: [ObjectId]
 });
 
 mongoose.model('Word', Word);
 
-exports.connect = function(connStr) {
-	mongoose.connect(connStr);
-};
-
-exports.Words = mongoose.model('Word');
+module.exports = {
+ 	Word: mongoose.model('Word'),
+	Game: mongoose.model('Game'),
+	connect: function(connStr) {
+		mongoose.connect(connStr);
+	}
+}
+	
